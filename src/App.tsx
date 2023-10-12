c1329b2002b836ba2745945fc4da67fce544ee7e
PedroPaivaDev <pedropaivadev@gmail.com>
2023-10-12 10:58:50 -0300

    class 201: TypeScript inference in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+
 function App() {
+  const [total, setTotal] = React.useState(0); //inferencia
+
+  function incrementar(event:React.MouseEvent) { //tipo declarado
+    setTotal((total) => total + 1);
+  }
+
   return (
-    <div>React</div>
-  )
+    <div>
+      <p>Total: {total}</p>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
