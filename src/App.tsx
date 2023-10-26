fc30dadcbaaba26ec732bbae3e07f4c4b02e4e6d
PedroPaivaDev <pedropaivadev@gmail.com>
2023-10-26 18:54:23 -0300

    class 301: Type annotation of a setState function that will be used within an 'onClick' event

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,44 @@ import Button from './Button';
 import Input from './Input';
 import Checkbox from './Checkbox';
 
+function user() {
+  return {
+    nome: 'André',
+    profissao: 'Designer'
+  }
+}
+
+type User = {
+  nome: string;
+  profissao: string;
+}
+
 function App() {
   const [total, setTotal] = React.useState(0); //inferencia
   const [date, setDate] = React.useState('');
+  const [data, setData] = React.useState<null | User>(null);
 
   function incrementar() { //tipo declarado
     setTotal((total) => total + 1);
   }
 
+  React.useEffect(() => {
+    setTimeout(() => {
+      setData(user());
+    },1000)
+  },[])
+
   return (
     <div>
       <p>Total: {total}</p>
       <Button
+        incrementar={setTotal}
         id='botao-principal'
         className='btn'
         onClick={incrementar}
         tamanho='1.5rem'
-        total={total}
-        setTotal={setTotal}
+        // total={total}
+        // setTotal={setTotal}
       >
         Incrementar
       </Button>
@@ -29,6 +49,10 @@ function App() {
       <Input value={date} onChange={(e) => setDate(e.currentTarget.value)} id='day' label='Dia' type='date' />
       <Input id='hour' label='Hora' type='time' />
       <Checkbox label='Termos e Condições'/>
+      {!data ?
+        <div>Carregando...</div> :
+        <div>{data.nome}: {data.profissao}</div>
+      }
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -9,19 +9,20 @@ type ButtonProps =
   React.ComponentProps<'button'> &  //utilitário genérico com extensão de props
   {
     tamanho?: string;
-    total: number;
-    setTotal: React.Dispatch<React.SetStateAction<number>>;
+    // total: number;
+    // setTotal: React.Dispatch<React.SetStateAction<number>>;
+    incrementar: React.Dispatch<React.SetStateAction<number>>;
   }
 ;
 
-const Button = ({tamanho, total, setTotal, children, ...props}:ButtonProps): React.ReactElement => {
+const Button = ({incrementar, tamanho, /* total, setTotal, */ children, ...props}:ButtonProps): React.ReactElement => {
   const handleClick: React.MouseEventHandler = (event: React.MouseEvent) => {
     // não é necessário tipar a função e o parâmetro, pois o TS já faz a inferência quando um dos dois é tipado
     console.log(event.pageX)
   }
   return (
     <button style={{fontSize: tamanho}}
-      onClick={handleClick}
+      onClick={() => incrementar(n => n + 1)}
       {...props}
     >
       {children}
