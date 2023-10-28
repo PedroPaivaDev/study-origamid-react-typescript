20740a3476fd97eda68e493374f455d93dd29a79
PedroPaivaDev <pedropaivadev@gmail.com>
2023-10-28 15:26:35 -0300

    class 303: useRef is used to manipulate DOM elements and can also receive generic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,24 @@
 import React from 'react';
-import Input from './Input';
 
-interface Sale {
-  data: string;
-  id: string;
-  nome: string;
-  pagamento: 'pix' | 'cartao' | 'boleto';
-  parcelas: number | null;
-  preco: number;
-  status: 'pago' | 'falha' | 'processando';
-}
+import videoSrc from './assets/video.mp4';
 
 function App() {
-  const [sales, setSales] = React.useState<Sale[]|null>(null);
-  const [initialDate, setInitialDate] = React.useState<string>('');
-  const [finalDate, setFinalDate] = React.useState<string>('');
-
-  async function getSales(inicio:string, final:string) {
-    const url = `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
-    const data = await fetch(url);
-    const json = data.json();
-    return json;
-  }
-
-  function useEffectCallback() {
-    console.log('Mountou');
-    return () => {
-      console.log('Desmontou');
-    };
-  }
-
-  React.useEffect(useEffectCallback, []);
+  const video = React.useRef<HTMLVideoElement>(null);
 
   React.useEffect(() => {
-    if(initialDate.length===0 || finalDate.length===0 ) return;
-    async function salesData(inicio:string, final:string) {
-      const data = await getSales(inicio, final)
-      setSales(data)
-    }
-    salesData(initialDate, finalDate)
-  },[initialDate, finalDate]);
+    console.log(video.current)
+  },[])
 
   return (
     <div>
-      <h1>Exercício da Aula 301</h1>
-      <br/>
-      <p>Defina a data de início e final, para visualizar o histórico de vendas nesse período.</p>
-      <Input setState={setInitialDate} label='Data de Início' type='date'/>
-      <Input setState={setFinalDate} label='Data Final' type='date'/>
-      <br/>
-      <h2>Histórico de Vendas:</h2>
-      {sales && sales.map(sale =>
-        <div key={sale.id}>
-          <h3>{sale.nome}</h3>
-          <p>R$ {sale.preco.toFixed(2)}</p>
-        </div>
-      )}
+      <h1>Exercício da Aula 303</h1>
+      <br />
+      <div className='flex'>
+        <button onClick={() => video.current?.play()}>Play</button>
+        <button onClick={() => video.current?.pause()}>Pause</button>
+      </div>
+      <br />
+      <video ref={video} src={videoSrc}></video>
     </div>
   );
 }
