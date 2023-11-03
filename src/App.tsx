f8bfeea1a12178097881db4262a6c53094b0f089
Pedro <pedropaivadev@gmail.com>
2023-11-03 18:56:12 -0300

    class 401: Preparing the environment to start the final course project

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,9 @@
 import React from 'react';
 
-import { UiContextProvider } from './contexts/UiContext';
-import { UserContext, UserContextProvider } from './contexts/UserContext';
-import useFetch from './hooks/useFetch';
-
-import Header from './components/Header';
-import Content from './components/Content';
-import Form from './components/Form';
-
-const usersUrl = 'https://data.origamid.dev/usuarios';
-
 function App() {
-  const users = useFetch<User[]>(usersUrl);
-  const [userId, setUserId] = React.useState<number>(1);
-
-  if(users.loading) {
-    return <p>Carregando...</p>
-  }
-
-  if(users.error) {
-    return <p>Ocorreu um erro: {users.error}</p>
-  }
 
   return (
     <div>
-      <Form />
-      <UserContextProvider userUrl={`${usersUrl}/${userId}`}>
-        <UiContextProvider>
-          <Header/>
-          <Content />
-          <div className='flex'>
-            {users && users.data?.map(user =>
-              <button
-                key={user.id}
-                onClick={() => setUserId(user.id)}
-              >
-                {user.nome}
-              </button>
-            )}
-          </div>
-        </UiContextProvider>
-      </UserContextProvider>
     </div>
   );
 }
