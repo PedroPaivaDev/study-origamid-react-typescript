a3f27f7267c86bbf1eb66a0f4f42bb2447655bd6
Pedro <pedropaivadev@gmail.com>
2023-11-04 13:50:47 -0300

    class 402: Home page structure prepared;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Resumo from './pages/Resumo';
+import Sidenav from './components/SideNav';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
     </div>
   );
 }
