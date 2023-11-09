e57d7e4e794e36556b6746bf792555f874522d0f
Pedro <pedropaivadev@gmail.com>
2023-11-09 20:10:25 -0300

    class 409: 'Resume' page iterates the data returned by the state of the data coming from the API and adds the filtered values according to the payment status of each item;

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
 import { useData } from '../contexts/DataContext';
 
 const Resumo = () => {
   const {data} = useData();
 
+  if(!data) return null;
+
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='resumo flex mb'>
+        <div className='box'>
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status!=='falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+            }
+          </span>
+        </div>
+        <div className='box'>
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status==='pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+            }
+          </span>
+        </div>
+        <div className='box'>
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status==='processando')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+            }
+          </span>
+        </div>
+      </div>
+      <div className='box'>Gráficos</div>
+    </section>
   )
 }
 
