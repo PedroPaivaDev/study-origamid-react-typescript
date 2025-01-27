96b9465b409197c03b3c947c671da2569bf2ef94
Pedro Paiva <pedropaivadev@gmail.com>
2025-01-27 18:53:28 -0300

    class 412: installing react-router-dom and creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { DataContextProvider } from './contexts/DataContext';
 import Sidenav from './components/Sidenav';
 import Header from './components/Header';
 import Resumo from './pages/Resumo';
 import Vendas from './pages/Vendas';
+import Venda from './pages/Venda';
 
 function App() {
 
   return (
-    <DataContextProvider>
-      <div className='container'>
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className='container'>
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
